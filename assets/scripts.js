d9836e6df998884b4e4c227847b8e13ad8a3eaf3
Muhammad Aziz Al-assad <muhazizal.26@gmail.com>
2020-04-05 23:15:32 +0700

    done

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,9 +1,29 @@
-// Toggle Icon for Navbar
-function toggleIcon() {
-  const navbar = document.querySelector('#nav-trigger');
+// Toggle Icon
+const navbar = document.querySelector('#nav-trigger');
+const toggle = document.querySelector('.icon-toggle');
+toggle.addEventListener('click', () => {
   if (navbar.className === 'navbar') {
-    navbar.className += ' responsive';
+    navbar.className += ' active';
   } else {
     navbar.className = 'navbar';
   }
+});
+
+// Button Read More
+const buttons = document.querySelectorAll('.btn-explore');
+for (btn of buttons) {
+  btn.addEventListener('click', () => {
+    alert('Page Under Maintance')
+  });
+}
+
+// Text Link
+const textLinks = document.querySelectorAll('.text-link');
+for (txt of textLinks) {
+  txt.addEventListener('mouseenter', (e) => {
+    e.target.style.textDecoration = 'underline';
+  });
+  txt.addEventListener('mouseleave', (e) => {
+    e.target.style.textDecoration = 'none';
+  });
 }

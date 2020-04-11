28ca84ab5890da030270a5250e8e1488b72fac6b
Muhammad Aziz Al-assad <muhazizal.26@gmail.com>
2020-04-11 15:33:49 +0700

    revisi

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,7 +1,7 @@
 // Toggle Icon
 const navbar = document.querySelector('#nav-trigger');
 const toggle = document.querySelector('.icon-toggle');
-toggle.addEventListener('click', () => {
+toggle.addEventListener('click', function () {
   if (navbar.className === 'navbar') {
     navbar.className += ' active';
   } else {
@@ -12,18 +12,18 @@ toggle.addEventListener('click', () => {
 // Button Read More
 const buttons = document.querySelectorAll('.btn-explore');
 for (btn of buttons) {
-  btn.addEventListener('click', () => {
-    alert('Page Under Maintance')
+  btn.addEventListener('click', function() {
+    alert('Page Under Maintance');
   });
 }
 
 // Text Link
 const textLinks = document.querySelectorAll('.text-link');
 for (txt of textLinks) {
-  txt.addEventListener('mouseenter', (e) => {
+  txt.addEventListener('mouseenter', function (e) {
     e.target.style.textDecoration = 'underline';
   });
-  txt.addEventListener('mouseleave', (e) => {
+  txt.addEventListener('mouseleave', function (e) {
     e.target.style.textDecoration = 'none';
   });
 }
